67c5fa51d6764d5d73cce53053269c7ee2c4eacd
Jacob Franz <jacobcfranz@gmail.com>
2020-06-14 15:46:47 -0500

    Initial upload of the SetCubeBuilder. It will currently pull the correct amount of each card and print it into a text document.

---
+++ README.md
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# SetCubeBuilder
+This will generate a set cube for Magic the Gathering by pulling data from scryfall.
+=======
+# setCubeBuilder
+This will generate a list of cards for purchasing from a given set to make it easier to purchase cards.
+>>>>>>> 54353fda30db05910f2c6772b45bc63b592ee484
---
+++ SetCubeBuilder.py
@@ -0,0 +1,89 @@
+import requests
+import json
+
+
+totalCards = 0
+
+def getNames(fetch, number, f):
+    #get the correct json blob
+
+    global totalCards
+
+    for x in fetch.items():
+        item = x
+
+    #switch to the list of cards rather than the first part of the blob
+    cardList = item[1]
+
+    #get an individual card
+    #card = cardList[0]
+    #print(type(card))
+
+    #f = open("cardnames.txt", "a+")
+
+    print(len(cardList))
+
+    for card in cardList:
+        #if card['promo_types'] != 'planeswalkerdeck':
+        #print(type(card))
+
+        if 'promo_types' not in card:
+            totalCards = totalCards + number
+            f.write(str(number))
+            f.write(' ')
+            f.write(card['name'])
+            f.write('\n')
+
+setCode = input('What set code corresponds to the set you are using for your cube? Ex: DOM ')
+print()
+
+
+f = open("cardnames.txt", "w+")
+
+response = requests.get('https://api.scryfall.com/cards/search?order=set&q=set%3A{}+rarity%3Am+not%3Apwdeck'.format(setCode))
+fetch = response.json()
+
+number = int(input("How many copies do you want of each Mythic? "))
+f.write('Mythics \n')
+getNames(fetch, number, f)
+f.write('\n')
+print()
+
+response = requests.get("https://api.scryfall.com/cards/search?order=set&q=set%3A{}+rarity%3Ar+not%3Apwdeck".format(setCode))
+fetch = response.json()
+
+number = int(input("How many copies do you want of each Rare? "))
+f.write('Rares \n')
+getNames(fetch, number, f)
+f.write('\n')
+print()
+
+response = requests.get("https://api.scryfall.com/cards/search?order=set&q=set%3A{}+rarity%3Au+not%3Apwdeck".format(setCube())
+fetch = response.json()
+
+number = int(input("How many copies do you want of each Uncommon? "))
+f.write('Uncommons \n')
+getNames(fetch, number, f)
+f.write('\n')
+print()
+
+response = requests.get("https://api.scryfall.com/cards/search?order=set&q=set%3A{}+rarity%3Ac+not%3Apwdeck+-t%3Abasic".format(setCube())
+fetch = response.json()
+
+number = int(input("How many copies do you want of each Common? "))
+f.write('Commons \n')
+getNames(fetch, number, f)
+f.write('\n')
+print('You need ' + str(totalCards) + ' cards.')
+
+
+f.close()
+
+
+
+
+
+
+
+
+
